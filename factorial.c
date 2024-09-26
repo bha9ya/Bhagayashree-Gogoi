2e903cea36be73887f4ee1a7369df2a2b57e5345
bha9ya <gogoibhagaya0711@gmail.com>
2024-09-26 10:00:47 +0530

    Add files via upload
    Date-26/9/2024
    Team 1,RollNo -1
    Assignment 3

---
+++ factorial.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+long int factorial(int n)
+{
+int i;
+long int prod=1;
+if(n>1)
+for(i=2;i<=n;++i)
+prod*=i;
+return (prod);
+}
+main()
+{
+int n;
+printf("\nn =");
+scanf("%d",&n);
+printf("\nn! =%1d",factorial(n));
+}
