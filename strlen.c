02d1b048db7b263188f7f70be856f33857d3f0a0
Bhagayashree Gogoi <gogoibhagaya0711@gmail.com>
2024-09-27 12:22:10 +0530

    String length

---
+++ strlen.c
@@ -0,0 +1,13 @@
+/*String Length*/
+/*Date-27-9-2024*/
+#include<stdio.h>
+main()
+{
+char fname[25];
+int x;
+printf("Enter the string:\n");
+scanf("%s",fname);
+x=strlen(fname);
+printf("String length is %d",x);
+
+}
