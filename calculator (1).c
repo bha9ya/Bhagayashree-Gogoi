b3850bc56a3d8ece54dbcb71dd71340ef396711d
Bhagayashree Gogoi <gogoibhagaya0711@gmail.com>
2024-09-28 19:49:52 +0530

    calculator (1).c

--- calculator (1).c
+++ calculator (1).c
@@ -1,70 +1,57 @@
 /*Assignment -1*/
-/*Date-26/9/2024*/
+/*Date-26.9.2024*/
 
 #include <stdio.h>
-int add(int a, int b) {
-    return a + b;
+int add(int x, int y) {
+    return x + y;
 }
-void subtract(int a, int b) {
-    printf("Result: %d\n", a - b);
+void sub(int x, int y) {
+    printf("Result: %d\n", x - y);
 }
-int multiply() {
-    int a, b;
-    printf("Enter two numbers for multiplication: ");
-    scanf("%d %d", &a, &b);
-    return a * b;
+int multi() {
+    int x, y;
+    printf("Enter two numbers for multiplication: \n");
+    scanf("%d %d", &x, &y);
+    return x* y;
 }
 
-void divide() {
-    int a, b;
-    printf("Enter two numbers for division: ");
-    scanf("%d %d", &a, &b);
-    if (b != 0)
-        printf("Result: %.2f\n", (float)a / b);
+void div() {
+    int x, y;
+    printf("Enter two numbers for division: \n");
+    scanf("%d %d", &x, &y);
+    if (y != 0)
+        printf("Result: %.2f\n", (float)x/ y);
     else
         printf("Error: Division by zero is not allowed.\n");
 }
 
 int main() {
-    int choice, a, b;
-    char cont;
-    
-    do {
-        printf("\nSimple Calculator\n");
-        printf("1. Addition\n");
-        printf("2. Subtraction\n");
-        printf("3. Multiplication\n");
-        printf("4. Division\n");
+    int ch, x, y;
+        printf("Enter 1 for Addition\n");
+        printf("Enter 2 for Subtraction\n");
+        printf("Enter 3 for Multiplication\n");
+        printf("Enter 4 for Division\n");
         printf("Enter your choice (1-4): ");
-        scanf("%d", &choice);
+        scanf("%d", &ch);
         
-        switch (choice) {
+        switch (ch) {
             case 1:
-                
-                printf("Enter two numbers for addition: ");
-                scanf("%d %d", &a, &b);
-                printf("Result: %d\n", add(a, b));
+                printf("Enter two numbers for addition:\n ");
+                scanf("%d %d", &x, &y);
+                printf("Result: %d\n", add(x, y));
                 break;
             case 2:
-                printf("Enter two numbers for subtraction: ");
-                scanf("%d %d", &a, &b);
-                subtract(a, b);
+                printf("Enter two numbers for subtraction:\ ");
+                scanf("%d %d", &x, &y);
+                sub(x, y);
                 break;
             case 3:
-                printf("Result: %d\n", multiply());
+                printf("Result: %d\n", multi());
                 break;
             case 4:
-                divide();
+                div();
                 break;
             default:
                 printf("Invalid choice! Please enter a number between 1 and 4.\n");
         }
-        
-        printf("\nDo you want to perform another operation? (y/n): ");
-        scanf(" %c", &cont);
-        
-    } while (cont == 'y' || cont == 'Y');
-    
-    printf("Thank you for using the calculator!\n");
-    return 0;
 }
